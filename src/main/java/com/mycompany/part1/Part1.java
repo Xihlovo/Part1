ab0305bd545e09036737c64c767791051554a897
Xihlovo <nkhwashuxihlovo@gmail.com>
2026-04-11 12:38:27 +0200

    Added My CheckPasswordComplexity Method

---
+++ src/main/java/com/mycompany/part1/Part1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.part1;
+
+/**
+ *
+ * @author nkhwa
+ */
+public class Part1 {
+
+     public static void main(String[] args) {
+        Login login = new Login();
+        
+        System.out.println("Testing: " + login.checkUserName("kyl_1"));
+    }
+}
+
+    
